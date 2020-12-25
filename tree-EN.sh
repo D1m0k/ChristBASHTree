a6afeeb6c952b069b5094f3b9fcb966f5655086d
military1 <mr.gusliakov@mail.ru>
2020-12-25 08:48:57 +0200

    Update tree-EN.sh

--- tree-EN.sh
+++ tree-EN.sh
@@ -32,8 +32,8 @@ for ((i=1; i<=2; i++))
 new_year=$(date +'%Y')
 let new_year++
 tput setaf 1; tput bold
-tput cup $lin $((c - 6)); echo MERRY CHRISTMAS
-tput cup $((lin + 1)) $((c - 10)); echo And lots of CODE in $new_year
+tput cup $lin $((c - 6)); echo С Новым Годом и Рождеством!
+tput cup $((lin + 1)) $((c - 10)); echo Профессиональных успехов и отдачи от затраченных усилий в $new_year
 let c++
 k=1
 
